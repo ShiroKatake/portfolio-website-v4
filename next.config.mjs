40e7bde03e01a9e74c517dd6b5660e1fb545b9d3
ShiroKatake <tgkhang262@gmail.com>
2024-07-13 19:24:30 +1000

    feat: project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+    domains: ['cdn.sanity.io']
+  },
+  compiler: {
+    styledComponents: true,
+  },
+}
 
 export default nextConfig;
