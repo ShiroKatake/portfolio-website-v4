529b91d5ccc4e47aafd72a717e12c5573fcfeeef
ShiroKatake <tgkhang262@gmail.com>
2024-07-13 20:16:24 +1000

    feat: setup styled components ssr and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Eczar, DM_Sans } from "next/font/google";
 import "./globals.css";
+import StyledComponentsRegistry from "@/lib/registry";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+});
+
+const eczar = Eczar({
+  subsets: ["latin"],
+  variable: "--font-eczar",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${dmSans.variable} ${eczar.variable}`}>
+      <body className={eczar.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
