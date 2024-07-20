2fc019bb5d06a1f5234bfac0bf538c4c1274c2a4
ShiroKatake <tgkhang262@gmail.com>
2024-07-20 16:01:56 +1000

    feat(global var): typography

---
+++ src/styles/typography.ts
@@ -0,0 +1,27 @@
+export const fontSize = {
+  heading: {
+    sm: "1.25rem",
+    md: "1.5rem",
+    lg: "2.25rem",
+    xl: "3rem",
+  },
+  body: {
+    sm: "0.75rem",
+    md: "1rem",
+    lg: "1.25rem",
+  },
+}
+
+export const lineHeight = {
+  heading: {
+    sm: "1.875rem",
+    md: "2.25rem",
+    lg: "3rem",
+    xl: "4.5rem",
+  },
+}
+
+export const fontWeight = {
+  normal: 400,
+  bold: 700,
+}
