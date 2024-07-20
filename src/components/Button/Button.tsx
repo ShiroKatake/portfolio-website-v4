522138e207015422448470126733678ba519f5d2
ShiroKatake <tgkhang262@gmail.com>
2024-07-20 14:56:29 +1000

    feat: toggle to swap project type display

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { StyledButton } from "./Button.styled";
-import { IoIosSend } from "react-icons/io";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   variant?: "primary" | "secondary";
+  icon?: React.ReactNode;
 }
 
-export const Button: React.FC<Props> = ({ children, variant, ...props }) => {
+export const Button: React.FC<Props> = ({
+  children,
+  icon,
+  variant,
+  ...props
+}) => {
   return (
     <StyledButton $variant={variant} {...props}>
-      <span className="icon">
-        <IoIosSend />
-      </span>
+      <span className="icon">{icon}</span>
       {children}
     </StyledButton>
   );
--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "../Button";
+import { HiGlobeAlt } from "react-icons/hi2";
+import { IoGameController } from "react-icons/io5";
+import { Flex } from "../Layout";
+import { spacing } from "@/styles/spacing";
 
 type ProjectType = "game" | "web";
 
@@ -11,19 +15,21 @@ interface Props {
 export const Toggle: React.FC<Props> = ({ children }) => {
   const [projectType, setProjectType] = useState<ProjectType>("web");
   return (
-    <>
+    <Flex gap={spacing.sm}>
       <Button
         variant={projectType === "web" ? "primary" : "secondary"}
         onClick={() => setProjectType("web")}
+        icon={<HiGlobeAlt />}
       >
-        Web
+        <span>Web</span>
       </Button>
       <Button
         variant={projectType === "game" ? "primary" : "secondary"}
         onClick={() => setProjectType("game")}
+        icon={<IoGameController />}
       >
-        Game
+        <span>Game</span>
       </Button>
-    </>
+    </Flex>
   );
 };
